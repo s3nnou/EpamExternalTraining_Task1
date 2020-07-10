952f78294abdd3f80564fd94098aaca9b82b54cf
Uladlen <s3nnou@gmail.com>
2020-07-10 11:48:20 +0300

    added triangle

--- task_2/Figure.cs
+++ task_2/Figure.cs
@@ -8,22 +8,44 @@ namespace task_2
 {
     public abstract class Figure
     {
-        public abstract int GetPerimeter();
-        public abstract int GetSquare();
+        public abstract double GetPerimeter();
+        public abstract double GetSquare();
     }
 
-    public class Square: Figure
+    public class Rectangle : Figure
     {
         public int A { get => A; set => A = value; }
         public int B { get => B; set => B = value; }
         public int C { get => C; set => C = value; }
         public int D { get => D; set => D = value; }
 
-        public override int GetPerimeter()
+        public override double GetPerimeter()
         {
-            return a * 4;
+            return (A + B) * 2 ;
         }
 
-
+        public override double GetSquare()
+        {
+            return A * B;
+        }
     }
+
+    public class Triangle : Figure
+    {
+        public int A { get => A; set => A = value; }
+        public int B { get => B; set => B = value; }
+        public int C { get => C; set => C = value; }
+
+        public override double GetPerimeter()
+        {
+            return A * 4;
+        }
+
+        public override double GetSquare()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }  
+
 }
