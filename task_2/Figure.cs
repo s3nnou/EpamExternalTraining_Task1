7cbee53cadbcf75ed20a2bf4d7bb97527b7eae5f
Uladlen <s3nnou@gmail.com>
2020-07-10 11:51:03 +0300

    added circle

--- task_2/Figure.cs
+++ task_2/Figure.cs
@@ -48,4 +48,18 @@ namespace task_2
         }
     }  
 
+    public class Circle: Figure
+    {
+        public int R { get => R; set => R = value; }
+
+        public override double GetPerimeter()
+        {
+            return 2 * Math.PI * R;
+        }
+
+        public override double GetSquare()
+        {
+            return Math.PI *Math.Pow( R, 2);
+        }
+    }
 }
