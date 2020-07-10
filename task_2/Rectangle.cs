b7bcc18a50ba7992119c7090a733818047654f37
Uladlen <s3nnou@gmail.com>
2020-07-10 14:48:55 +0300

    Введите сообщение

--- task_2/Rectangle.cs
+++ task_2/Rectangle.cs
@@ -1,4 +1,6 @@
-﻿namespace FiguresClassLibrary
+﻿using System;
+
+namespace FiguresClassLibrary
 {
     public class Rectangle : Figure
     {
@@ -7,12 +9,22 @@
         private int _c;
         private int _d;
 
+        public Rectangle(int a, int b, int c, int d)
+        {
+
+        }
+
+        public Rectangle(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
+        {
+            _a = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2);
+
+        }
 
 
-        public int A { get => A; set => A = value; }
-        public int B { get => B; set => B = value; }
-        public int C { get => C; set => C = value; }
-        public int D { get => D; set => D = value; }
+        public int A { get => _a; set => _a = value; }
+        public int B { get => _b; set => _b = value; }
+        public int C { get => _c; set => _c = value; }
+        public int D { get => _d; set => _d = value; }
 
         public override double GetPerimeter()
         {
