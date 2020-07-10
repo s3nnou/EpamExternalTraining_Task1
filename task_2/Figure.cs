782505d246b1750ae7cdfeb04f01477e6b1faccb
Uladlen <s3nnou@gmail.com>
2020-07-10 11:35:44 +0300

    starting task 1.2

---
+++ task_2/Figure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    public abstract class Figure
+    {
+        public abstract int GetPerimeter();
+        public abstract int GetSquare();
+    }
+
+    public class Square: Figure
+    {
+        public int A { get => A; set => A = value; }
+        public int B { get => B; set => B = value; }
+        public int C { get => C; set => C = value; }
+        public int D { get => D; set => D = value; }
+
+        public override int GetPerimeter()
+        {
+            return a * 4;
+        }
+
+
+    }
+}
